815929344aa9331a6ec7fc0fd61b387ba1991c2a
Aio <AioDev.JS@gmail.com>
2023-08-07 09:12:24 +0900

    temp

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,47 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import { NextPageContext } from "next";
+import type { AppProps } from "next/app";
+import { useState } from "react";
+import {
+  Hydrate,
+  QueryClient,
+  QueryClientProvider,
+  dehydrate,
+} from "react-query";
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+export default function App({
+  Component,
+  pageProps,
+  dehydratedState,
+}: AppProps) {
+  const [queryClient] = useState(() => new QueryClient());
+  console.log("pageProps", dehydratedState);
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Hydrate state={dehydratedState}>
+        <Component {...pageProps} />
+      </Hydrate>
+    </QueryClientProvider>
+  );
 }
+
+App.getInitialProps = async (context: NextPageContext) => {
+  const requiredResources = context.Component.requiredResources;
+  const queryClient = new QueryClient();
+
+  if (requiredResources?.length > 0) {
+    await Promise.all(
+      requiredResources.map(async (resource: any) => {
+        return queryClient.fetchQuery({
+          queryKey: resource.key,
+          queryFn: resource.fetcher,
+          staleTime: Infinity,
+        });
+      }),
+    );
+  }
+
+  return {
+    dehydratedState: dehydrate(queryClient),
+  };
+};
